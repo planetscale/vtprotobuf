d55ce7d8b1f5469d78085ee188b7fe5f139ca025
Eko Adi Wicaksono <eko.adi.wicaksono@gmail.com>
2022-01-12 09:58:18 +0700

    patch for oneof
    Signed-off-by: Eko Adi Wicaksono <eko.adi.wicaksono@gmail.com>

--- features/unmarshal/unmarshal.go
+++ features/unmarshal/unmarshal.go
@@ -530,7 +530,11 @@ func (p *unmarshal) fieldItem(field *protogen.Field, fieldname string, message *
 			p.P(`if oneof, ok := m.`, fieldname, `.(*`, field.GoIdent, `); ok {`)
 			p.decodeMessage("oneof."+field.GoName, buf, field.Message)
 			p.P(`} else {`)
-			p.P(`v := &`, msgname, `{}`)
+			if p.ShouldPool(message) && p.ShouldPool(field.Message) {
+				p.P(`v := `, msgname, `FromVTPool()`)
+			} else {
+				p.P(`v := &`, msgname, `{}`)
+			}
 			p.decodeMessage("v", buf, field.Message)
 			p.P(`m.`, fieldname, ` = &`, field.GoIdent, `{v}`)
 			p.P(`}`)
