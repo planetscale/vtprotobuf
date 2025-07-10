a08e81ef3f151019b3bb0773a91f621a1ffb4e02
Jun Song <jun.song@finxact.com>
2025-07-10 11:36:13 -0400

    add protobuf editions support

--- cmd/protoc-gen-go-vtproto/main.go
+++ cmd/protoc-gen-go-vtproto/main.go
@@ -14,6 +14,8 @@ import (
 	"github.com/planetscale/vtprotobuf/generator"
 
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
@@ -33,6 +35,10 @@ func main() {
 	f.StringVar(&cfg.BuildTag, "buildTag", "", "the go:build tag to set on generated files")
 
 	protogen.Options{ParamFunc: f.Set}.Run(func(plugin *protogen.Plugin) error {
+		plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_SUPPORTS_EDITIONS)
+		plugin.SupportedEditionsMinimum = descriptorpb.Edition_EDITION_PROTO3
+		plugin.SupportedEditionsMaximum = descriptorpb.Edition_EDITION_MAX
+
 		gen, err := generator.NewGenerator(plugin, strings.Split(features, "+"), &cfg)
 		if err != nil {
 			return err
